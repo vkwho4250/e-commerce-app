aff6e7761612b518610ecfb5c150a413e31dd2b4
vkwho9ar <vkwho9ar@gmail.com>
2021-06-23 18:35:10 -0400

    created NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { ReactComponent as Logo } from "../assets/shared/desktop/logo.svg";
+import { ReactComponent as ShoppingCart } from "../assets/shared/desktop/icon-cart.svg";
 
 export default function Navbar() {
   return (
     <nav id="nav-bar">
-      <Logo />
+      <Logo className="svg-icons" />
       <div>
         <a href="#">Home</a>
         <a href="#">Headphones</a>
         <a href="#">Speakers</a>
         <a href="#">Earphones</a>
+      </div>
+      <div className="svg-icons">
+        <ShoppingCart />
       </div>
     </nav>
   );
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,10 @@
 import React from "react";
+import homeBannerImg from "../../assets/home/desktop/image-hero.jpg";
 
 export default function Home() {
   return (
     <div id="home-page">
+      <img src={homeBannerImg} id="home-banner-img" alt="homepage banner"></img>
       <h1>text</h1>
     </div>
   );
